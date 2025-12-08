6b61e7dcdf2e7fafacabff7252ae28f45620230f
waylon <rakamiso575@gmail.com>
2025-12-08 15:59:01 +0800

    init

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from langchainlearning!")
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
 
-
-if __name__ == "__main__":
-    main()
+# 加载环境变量
+load_dotenv()
+chatLLM = ChatOpenAI(
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    base_url=os.getenv("DASHSCOPE_API_URL"),
+    model="qwen-plus",  # 此处以qwen-plus为例，您可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+    # other params...
+)
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "你是谁？"}]
+response = chatLLM.invoke(messages)
+print(response.model_dump_json())
